d4d83bc40d60493c33540f2a85164efe59580b64
Theodor M <e1426389@student.tuwien.ac.at>
2019-09-03 15:32:13 +0200

    Reworked stlink hardware detection.
    Differentiate between float/high/low. Factored hardware specific setup out and moved to bottom. Introduced Possibility of not recognized Hardware. Added 'baite' Variant.

--- src/platforms/stlink/stlink_common.c
+++ src/platforms/stlink/stlink_common.c
@@ -21,14 +21,28 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
 
-/* return 0 for stlink V1, 1 for stlink V2 and 2 for stlink V2.1 */
+/* returns one of the following values to indicate hardware:
+ * 0: ST-Link V1, e.g. on VL Discovery
+ * 1: ST-Link V2, e.g. on F4 Discovery or ST-Link "baite" clones
+ * 2: ST-Link V2.1
+ * 255: not recognized (alternatively mapped to a default!)
+ *
+ * Detection is done reading gpio values.
+ *
+ * WARNING:
+ * Performs hw specific setup
+ */
+
 uint32_t detect_rev(void)
 {
 	uint32_t rev;
-	int res;
 
-	while (RCC_CFGR & 0xf) /* Switch back to HSI. */
+	/* Test for HSI Clock, switch to HSI if necessary */
+	while (RCC_CFGR & 0xf) {
 		RCC_CFGR &= ~3;
+	}
+
+	/* Enable all Peripheral Clocks */
 	rcc_periph_clock_enable(RCC_GPIOA);
 	rcc_periph_clock_enable(RCC_GPIOB);
 	rcc_periph_clock_enable(RCC_GPIOC);
@@ -36,54 +50,181 @@ uint32_t detect_rev(void)
 	rcc_periph_reset_pulse(RST_USB);
 	rcc_periph_clock_enable(RCC_AFIO);
 	rcc_periph_clock_enable(RCC_CRC);
-	/* First, get Board revision by pulling PC13/14 up. Read
-	 *  11 for ST-Link V1, e.g. on VL Discovery, tag as rev 0
-	 *  00 for ST-Link V2, e.g. on F4 Discovery, tag as rev 1
-	 *  01 for ST-Link V2, else,                 tag as rev 1
-	 */
-	gpio_set_mode(GPIOC, GPIO_MODE_INPUT,
-				  GPIO_CNF_INPUT_PULL_UPDOWN, GPIO14 | GPIO13);
-	gpio_set(GPIOC, GPIO14 | GPIO13);
-	for (int i = 0; i < 100; i ++)
-		res = gpio_get(GPIOC, GPIO13);
-	if (res)
-		rev = 0;
-	else {
-		/* Check for V2.1 boards.
-		 * PA15/TDI is USE_RENUM, pulled with 10 k to U5V on V2.1,
-		 * Otherwise unconnected. Enable pull low. If still high.
-		 * it is V2.1.*/
-		rcc_periph_clock_enable(RCC_AFIO);
-		AFIO_MAPR |= 0x02000000; /* Release from TDI.*/
-		gpio_set_mode(GPIOA, GPIO_MODE_INPUT,
-                                 GPIO_CNF_INPUT_PULL_UPDOWN, GPIO15);
+	AFIO_MAPR |= 0x02000000; /* Release from TDI such that PA15 can be read */
+
+#define READ_WITH_PULLUP(MVAR,MPORT,MPIN) \
+	do {\
+		gpio_set_mode(MPORT,GPIO_MODE_INPUT,GPIO_CNF_INPUT_PULL_UPDOWN,MPIN);\
+		gpio_set(MPORT,MPIN);\
+		for (int i=0; i<100; i++) {\
+			MVAR = gpio_get(MPORT,MPIN);\
+		}\
+	} while (0)
+
+#define READ_WITH_PULLDOWN(MVAR,MPORT,MPIN) \
+	do {\
+		gpio_set_mode(MPORT,GPIO_MODE_INPUT,GPIO_CNF_INPUT_PULL_UPDOWN,MPIN);\
+		gpio_clear(MPORT,MPIN);\
+		for (int i=0; i<100; i++) {\
+			MVAR = gpio_get(MPORT,MPIN);\
+		}\
+	} while (0)
+
+	/*
+		The code has the following pattern:
+			read_input_to_variable();
+			if (is_floating) { ... }
+			else if (is_pulled low) { ... }
+			else { ... }
+	*/
+
+	/* read PC13 */
+	int pc_13_pup, pc_13_pud;
+	READ_WITH_PULLUP(pc_13_pup,GPIOC,GPIO13);
+	READ_WITH_PULLDOWN(pc_13_pud,GPIOC,GPIO13);
+
+	if (pc_13_pup != pc_13_pud) {
+		/* PC13 floating */
+
+		/* check for PB11 which is pulled high by SWIM circutry
+			in baite Variant and not connected on VL Discovery */
+
+		/* read PB11 */
+		int pb_11_pup, pb_11_pud;
+		READ_WITH_PULLUP(pb_11_pup,GPIOB,GPIO11);
+		READ_WITH_PULLDOWN(pb_11_pud,GPIOB,GPIO11);
+
+		if (pb_11_pup != pb_11_pud) {
+			/* PB11 floating */
+			rev = 0; /* ST-Link V1 on VL Discovery */
+		} else if (pb_11_pup == 0) {
+			/* PB11 not floating and pulled low externally */
+			rev = 0xFF; /* unknown variant */
+		} else {
+			/* PB11 not floating and pulled high externally */
+			rev = 1; /* ST-Link V2 "baite" */
+		}
+
+	} else if (pc_13_pup == 0) {
+		/* PC13 not floating and pulled low externally */
+
+		/* read PC14 */
+		int pc_14_pup, pc_14_pud;
+		READ_WITH_PULLUP(pc_14_pup,GPIOC,GPIO14);
+		READ_WITH_PULLDOWN(pc_14_pud,GPIOC,GPIO14);
+
+		if (pc_14_pup != pc_14_pud) {
+			/* PC14 floating */
+
+			/* Check for V2.1 boards. PA15/TDI is USE_RENUM,
+				pulled with 10 k to U5V on V2.1, otherwise unconnected. */
+
+			/* read PA15 */
+			int pa_15_pup, pa_15_pud;
+			READ_WITH_PULLUP(pa_15_pup,GPIOA,GPIO15);
+			READ_WITH_PULLDOWN(pa_15_pud,GPIOA,GPIO15);
+
+		 	if (pa_15_pup != pa_15_pud) {
+				/* PA15 floating */
+				rev = 1; /* ST-Link V2 */
+			} else if (pa_15_pup == 0) {
+				/* PA15 not floating and pulled low externally */
+				rev = 0xFF; /* unknown variant */
+			} else {
+				/* PA15 not floating and pulled high externally */
+				rev = 2; /* ST-Link V2.1 */
+			}
+
+		} else if (pc_14_pup == 0) {
+			/* PC14 not floating and pulled low externally */
+			rev = 1; /* ST-Link V2 on Legacy Discovery F4xx */
+		} else {
+			/* PC14 not floating and pulled high externally */
+			rev = 0xFF; /* unknown variant */
+		}
+
+	} else {
+		/* PC13 not floating and pulled high externally */
+		rev = 0xFF; /* unknown variant */
+	}
+
+	#define STLINK_DEFAULT_ON_UNKNOWN (1)
+	#ifdef STLINK_DEFAULT_ON_UNKNOWN
+	{
+		/* check if one of the known variants is selected, otherewise default */
+		if (rev == 0xFF) {
+			rev = STLINK_DEFAULT_ON_UNKNOWN;
+		}
+	}
+	#endif
+
+	/* apply revision specific configuration */
+	/* TODO: is there a more appropriate place for this? */
+
+	if (rev == 0 || rev == 1) {
+		/* PA12: open-drain, pulled low (USB-Data-Plus) */
+		gpio_clear(GPIOA, GPIO12);
+		gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_2_MHZ,
+			GPIO_CNF_OUTPUT_OPENDRAIN, GPIO12);
+	}
+
+	if (rev == 2) {
+		/* PB15: Pull PWR_ENn low. */
+		gpio_set_mode(GPIOB, GPIO_MODE_OUTPUT_2_MHZ,
+			GPIO_CNF_OUTPUT_OPENDRAIN, GPIO15);
+		gpio_clear(GPIOB, GPIO15);
+		/* PA15: Pull USB_RENUM low. */
+		gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_2_MHZ,
+			GPIO_CNF_OUTPUT_OPENDRAIN, GPIO15);
 		gpio_clear(GPIOA, GPIO15);
-		for (int i = 0; i < 100; i++)
-			res =  gpio_get(GPIOA, GPIO15);
-		if (res) {
-			rev = 2;
-			/* Pull PWR_ENn low.*/
-			gpio_set_mode(GPIOB, GPIO_MODE_OUTPUT_2_MHZ,
-						  GPIO_CNF_OUTPUT_OPENDRAIN, GPIO15);
-			gpio_clear(GPIOB, GPIO15);
-			/* Pull USB_RENUM low!*/
-			gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_2_MHZ,
-						  GPIO_CNF_OUTPUT_OPENDRAIN, GPIO15);
-			gpio_clear(GPIOA, GPIO15);
-		} else
-			/* Catch F4 Disco board with both resistors fitted.*/
-			rev = 1;
+	}
+
+	if (rev == 1 || rev == 2) {
 		/* On Rev > 0 unconditionally activate MCO on PORTA8 with HSE! */
 		RCC_CFGR &= ~(0xf << 24);
 		RCC_CFGR |= (RCC_CFGR_MCO_HSE << 24);
 		gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_50_MHZ,
-		GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO8);
+			GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO8);
 	}
-	if (rev < 2) {
-		gpio_clear(GPIOA, GPIO12);
-		gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_2_MHZ,
-					  GPIO_CNF_OUTPUT_OPENDRAIN, GPIO12);
+
+	#define STLINK_DEBUG_HW_DETECTION
+	#ifdef STLINK_DEBUG_HW_DETECTION
+	{
+		/* read all pins anyways */
+		int pa_15_pup, pa_15_pud;
+		int pb_11_pup, pb_11_pud;
+		int pc_14_pup, pc_14_pud;
+		int pc_13_pup, pc_13_pud;
+		READ_WITH_PULLUP(pa_15_pup,GPIOA,GPIO15);
+		READ_WITH_PULLDOWN(pa_15_pud,GPIOA,GPIO15);
+		READ_WITH_PULLUP(pb_11_pup,GPIOB,GPIO11);
+		READ_WITH_PULLDOWN(pb_11_pud,GPIOB,GPIO11);
+		READ_WITH_PULLUP(pc_14_pup,GPIOC,GPIO14);
+		READ_WITH_PULLDOWN(pc_14_pud,GPIOC,GPIO14);
+		READ_WITH_PULLUP(pc_13_pup,GPIOC,GPIO13);
+		READ_WITH_PULLDOWN(pc_13_pud,GPIOC,GPIO13);
+		/*
+			encode gpio readings into revision
+			lower 16 bit are still revision
+			upper 16 bit are 1 nibble per pin, interpration is: as follows:
+				0x3 -> externally pulled high
+				0x2 -> basically impossible (pin inverts internal pull)
+				0x1 -> floating
+				0x0 -> externally
+		*/
+		rev = (rev&0xFF)
+			| ((pa_15_pup!=0)?(1<<16):0)
+			| ((pa_15_pud!=0)?(1<<17):0)
+			| ((pb_11_pup!=0)?(1<<20):0)
+			| ((pb_11_pud!=0)?(1<<21):0)
+			| ((pc_14_pup!=0)?(1<<24):0)
+			| ((pc_14_pud!=0)?(1<<25):0)
+			| ((pc_13_pup!=0)?(1<<28):0)
+			| ((pc_13_pud!=0)?(1<<29):0)
+			;
 	}
+	#endif
+
 	return rev;
 }
 
